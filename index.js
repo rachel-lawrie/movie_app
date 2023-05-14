d0ee63681f0c25c077eff1d339d91a0562b9a9ca
rachel-lawrie <124215524+rachel-lawrie@users.noreply.github.com>
2023-05-14 14:51:46 -0700

    removed authentication from /movies endpoint

--- index.js
+++ index.js
@@ -60,20 +60,16 @@ app.get("/", (req, res) => {
 });
 
 // Return a list of ALL movies to the user
-app.get(
-  "/movies",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    Movies.find()
-      .then((movies) => {
-        res.status(201).json(movies);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error" + err);
-      });
-  }
-);
+app.get("/movies", (req, res) => {
+  Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error" + err);
+    });
+});
 
 // Return data on a single movie by movie title
 app.get(
