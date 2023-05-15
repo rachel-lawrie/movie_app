da7131757475b826f99af13b20277bfab726269f
rachel-lawrie <124215524+rachel-lawrie@users.noreply.github.com>
2023-05-15 16:55:03 -0700

    Reput in check origin

--- index.js
+++ index.js
@@ -32,6 +32,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require("cors");
 
+let allowedOrigins = [
+  "http://localhost:8080",
+  "http://testsite.com",
+  "http://localhost:1234",
+];
+
 function checkOrigin(origin, callback) {
   if (!origin) return callback(null, true);
   if (allowedOrigins.indexOf(origin) === -1) {
